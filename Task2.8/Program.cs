beac277085b4fd49fae213edaaecacd519bf1801
Maksim <maksimus.tumannick@yandex.ru>
2022-06-24 22:45:57 +0300

    Task2.8

---
+++ Task2.8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "12345";
+            int tryCount = 3;
+
+            for (int i = 0; i <= tryCount; tryCount--)
+            {
+                Console.WriteLine("Введите пароль");
+                string inputWordByUser = Console.ReadLine();
+
+                if(inputWordByUser == password)
+                {
+                    Console.WriteLine("Секретная фраза");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Неверно! У Вас осталось {tryCount} попыток");
+                }
+            }
+        }
+    }
+}
